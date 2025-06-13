088e18cd8b671f7179ef2bc483052d9582513164
alarik84 <119766480+alarik84@users.noreply.github.com>
2025-06-14 01:34:12 +0200

    Add files via upload

---
+++ include/clientSecrets.h
@@ -0,0 +1,9 @@
+const char*  wifiSsid = "Provide your ssid here";
+const char*  wifiPassword = "Provide your password here";
+
+const char* mqttServer = "Provide your http or ip here, ip should be provided withouut quotas"; //"172.18.0.3";
+const int  mqttPort = 1883;
+const char* mqttUser = "provide your user name for mqtt here";
+const char* mqttPasswd = "provide your password to mqtt here";
+
+const char* otaPassword = "provide your OTA password here";
